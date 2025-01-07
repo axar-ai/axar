c86c02ff02f046ef8d33cecdeb6597516d7b2579
Sudipta Ghosh <uadayghosh@gmail.com>
2025-01-07 19:55:36 +0600

    feat: new airline example added

--- examples/airline/airline-agent.ts
+++ examples/airline/airline-agent.ts
@@ -51,6 +51,9 @@ async function main() {
   const cancelQuery2 =
     "I want to cancel my flight. I can't make it anymore due to a personal conflict, and I want refund";
 
+  const cancelQuery3 =
+    "I have another connecting flight. I want to cancel my flight. I can't make it anymore due to a personal conflict, and I want refund";
+
   const unclearQuery =
     'I dont want this flight, please reschedule it for next week and provide me the new flight details.';
   const lostBaggageQuery = 'My bag is missing please help me find it';
@@ -75,6 +78,15 @@ async function main() {
     await triggerAgent.run(
       JSON.stringify({
         query: cancelQuery2,
+        context: context,
+      }),
+    ),
+  );
+
+  console.log(
+    await triggerAgent.run(
+      JSON.stringify({
+        query: cancelQuery3,
         context: context,
       }),
     ),
